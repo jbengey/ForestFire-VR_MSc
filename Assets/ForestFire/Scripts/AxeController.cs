9fa9972495533ac4fe0b612a7dee488b39c0a431
JBengey <jack.bengeyfield@gmail.com>
2020-10-24 13:08:05 +0100

    Created test scene Implemented base Tree Health logic Implemented base Axe controller logic

---
+++ Assets/ForestFire/Scripts/AxeController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeController : MonoBehaviour
+{
+
+    public AudioSource TreeChopSound1;
+    GameObject Axe;
+
+    private void Start()
+    {
+        Axe = this.gameObject;
+    }
+
+    //Function to enable to gravity on the rigid body, but only once the first grab has occured - allows axe to stay stuck into tree at the start of the scene
+    public void EnaleGravity()
+    {
+        Rigidbody axeRB = Axe.GetComponent<Rigidbody>();
+        axeRB.useGravity = true;
+    }
+
+    //Function to disable gravity on the rigid body, useful if wanting the Axe to become stuck in a tree
+    public void DisableGravity()
+    {
+        Rigidbody axeRB = Axe.GetComponent<Rigidbody>();
+        axeRB.useGravity = false;
+    }
+
+
+
+    //Method to call on collision of Axe 
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject parentCell = collision.gameObject.transform.parent.gameObject.transform.parent.gameObject.transform.parent.gameObject; //Create reference to the ForestFireCell prefab this instance is childed to - so that I can access the current state of the tree (alight, alive etc..)
+        ForestFireCell activeCell = parentCell.GetComponent<ForestFireCell>();
+
+
+        if ((collision.gameObject.tag == "tree") & (activeCell.cellState == ForestFireCell.State.Tree))
+        {
+            TreeChopSound1.Play(); //Play the sound provided
+
+            Debug.Log("Collison with " + collision.gameObject.transform.parent.gameObject.name); //Debug the collision to determine whether the gameobect was colliding with the parent or child instance
+
+            GameObject parentTree = collision.gameObject.transform.parent.gameObject; //Create a gameobject variable to store the parent of the gameobject it collided with - where treecontroller is located
+            TreeController activeTree = parentTree.GetComponent<TreeController>(); //Find Tree controller component, to allow access to variables
+            activeTree.treeHealth--; //Decrement tree health by 1
+
+        }
+    }
+}
---
+++ Assets/ForestFire/Scripts/TreeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class TreeController : MonoBehaviour
+{
+    //Variables creation
+    GameObject myTree;
+    public int treeHealth=3;
+    private bool fallen = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTree = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (treeHealth <=0 & fallen==false)
+        {
+            Rigidbody treeRB = myTree.AddComponent<Rigidbody>();
+            treeRB.isKinematic = false;
+            treeRB.useGravity = true;
+            treeRB.AddForce(Vector3.forward, ForceMode.Impulse);
+            StartCoroutine(removeTree());
+            fallen = true;
+
+        }
+    }
+
+    //Create corutine
+    private IEnumerator removeTree()
+    {
+        yield return new WaitForSeconds(8);
+        Destroy(myTree);
+    }
+
+
+
+
+}
